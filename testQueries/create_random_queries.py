dc436de4a6065ff37846dd1ed2a8c89db10e0a39
Matt Harding <mharding15@gmail.com>
2020-12-23 02:30:14 -0500

    added 9 test query files for the fly dataset (E-CURD-21)

--- testQueries/create_random_queries.py
+++ testQueries/create_random_queries.py
@@ -2,37 +2,72 @@ import generate_query as gq
 
 def createQueryFiles():
 
-	fewClauses = 10
-	manyClauses = 100
-	fewTermsMin = 5
-	fewTermsMax = 15
-	manyTermsMin = 90
-	manyTermsMax = 110
-	numGenes = 19000
+	# refers to the number of clauses that will be in the queries
+	smallClauses = 5
+	mediumClauses = 20
+	largeClauses = 100
 
-	f = open('fewClauses_fewTerms', 'w')
-	for i in range(5):
-		f.write(gq.randomQuery(numGenes, fewClauses, fewTermsMin, fewTermsMax) + "\n")
+	# these mins and maxs refer to how many genes will randomly be in a given clause
+	smallTermsMin = 5
+	smallTermsMax = 15
 
+	mediumTermsMin = 5
+	mediumTermsMax = 15
+
+	largeTermsMin = 15
+	largeTermsMax = 20
+
+	# total number of genes
+	numGenes = 9503
+
+	# number of queries that will go in each file
+	numQueries = 10
+
+	# E-CURD-21 is the name of the fly data files
+	f = open('E-CURD-21-random-queries-small-clauses-small-genes.in', 'w')
+	for i in range(numQueries):
+		f.write(gq.randomQuery(numGenes, smallClauses, smallTermsMin, smallTermsMax) + "\n")
 	f.close()
 
-	f = open('fewClauses_manyTerms', 'w')
-	for i in range(5):
-		f.write(gq.randomQuery(numGenes, fewClauses, manyTermsMin, manyTermsMax) + "\n")
-
+	f = open('E-CURD-21-random-queries-small-clauses-medium-genes.in', 'w')
+	for i in range(numQueries):
+		f.write(gq.randomQuery(numGenes, smallClauses, mediumTermsMin, mediumTermsMax) + "\n")
 	f.close()
 
-	f = open('manyClauses_fewTerms', 'w')
-	for i in range(5):
-		f.write(gq.randomQuery(numGenes, manyClauses, fewTermsMin, fewTermsMax) + "\n")
-
-	f.close()
-
-	f = open('manyClauses_manyTerms', 'w')
-	for i in range(5):
-		f.write(gq.randomQuery(numGenes, manyClauses, manyTermsMin, manyTermsMax) + "\n")
-
+	f = open('E-CURD-21-random-queries-small-clauses-large-genes.in', 'w')
+	for i in range(numQueries):
+		f.write(gq.randomQuery(numGenes, smallClauses, largeTermsMin, largeTermsMax) + "\n")
 	f.close()
 
 
+	f = open('E-CURD-21-random-queries-medium-clauses-small-genes.in', 'w')
+	for i in range(numQueries):
+		f.write(gq.randomQuery(numGenes, mediumClauses, smallTermsMin, smallTermsMax) + "\n")
+	f.close()
+
+	f = open('E-CURD-21-random-queries-medium-clauses-medium-genes.in', 'w')
+	for i in range(numQueries):
+		f.write(gq.randomQuery(numGenes, mediumClauses, mediumTermsMin, mediumTermsMax) + "\n")
+	f.close()
+
+	f = open('E-CURD-21-random-queries-medium-clauses-large-genes.in', 'w')
+	for i in range(numQueries):
+		f.write(gq.randomQuery(numGenes, mediumClauses, largeTermsMin, largeTermsMax) + "\n")
+	f.close()
+
+
+	f = open('E-CURD-21-random-queries-large-clauses-small-genes.in', 'w')
+	for i in range(numQueries):
+		f.write(gq.randomQuery(numGenes, largeClauses, smallTermsMin, smallTermsMax) + "\n")
+	f.close()
+
+	f = open('E-CURD-21-random-queries-large-clauses-medium-genes.in', 'w')
+	for i in range(numQueries):
+		f.write(gq.randomQuery(numGenes, largeClauses, mediumTermsMin, mediumTermsMax) + "\n")
+	f.close()
+
+	f = open('E-CURD-21-random-queries-large-clauses-large-genes.in', 'w')
+	for i in range(numQueries):
+		f.write(gq.randomQuery(numGenes, largeClauses, largeTermsMin, largeTermsMax) + "\n")
+	f.close()
 
